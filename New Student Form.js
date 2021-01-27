f4fc5bc93b5867261306e35ea594c3049f2680b0
Matthew15243 <matthew@amplifycp.com>
2021-01-27 08:36:25 -0700

    js push

--- New Student Form.js
+++ New Student Form.js
@@ -1,1 +1,17 @@
-ellipse(50, 50, 50, 50);
+// https://stackoverflow.com/questions/8714090/queryselector-wildcard-element-match
+//   Queryselector wild cards
+/*********************************************************************
+ * Description:
+ *   This will add a new sibling + age label and input tag
+ ********************************************************************/
+function addSibling()
+{
+    let siblings = document.querySelectorAll("label[for^=\"sibling\"]");
+    let siblingCount = siblings.length / 2;
+    console.log(siblingCount);
+}
+addSibling()
+            //<label for = "sibling1" class = "label">Sibling:</label>
+            //<input type="text" id = "sibling1" class = "input">
+            //<label for = "siblingAge1" class = "label">Age:</label>
+            //<input type="number" min = 1 max = 130 id = "siblingAge1" class = "input"></input>
