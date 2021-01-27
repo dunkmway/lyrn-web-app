9ec7704d58ac34092d3e8a9d4afa70d211e04f8f
Matthew15243 <matthew@amplifycp.com>
2021-01-27 15:54:04 -0700

    JS for adding a sibling (still need a listener)

--- New Student Form.js
+++ New Student Form.js
@@ -6,9 +6,44 @@
  ********************************************************************/
 function addSibling()
 {
+    // Count how many siblings are there already and create their id: siblingName and siblingAge
     let siblings = document.querySelectorAll("label[for^=\"sibling\"]");
     let siblingCount = siblings.length / 2;
-    console.log(siblingCount);
+    let siblingName = "sibling" + String(siblingCount);
+    let siblingAge  = "siblingAge" + String(siblingCount);
+
+    let siblingArray = [];
+    
+    // Create the new labels and inputs
+    let siblingLabel = document.createElement("label")
+    siblingLabel.setAttribute("for", "sibling" + String(siblingCount + 1))
+    siblingLabel.innerHTML = "Sibling:"
+    siblingLabel.className = "label"
+    siblingArray.push(siblingLabel);
+
+    let siblingInput = document.createElement("input")
+    siblingInput.setAttribute("id", "sibling" + String(siblingCount + 1))
+    siblingInput.className = "input"
+    siblingArray.push(siblingInput);
+
+    let siblingAgeLabel = document.createElement("label")
+    siblingLabel.setAttribute("for", "siblingAge" + String(siblingCount + 1))
+    siblingAgeLabel.innerHTML = "Age:"
+    siblingAgeLabel.className = "label"
+    siblingArray.push(siblingAgeLabel);
+
+    let siblingAgeInput = document.createElement("input")
+    siblingAgeInput.setAttribute("id", "siblingAge" + String(siblingCount + 1))
+    siblingAgeInput.className = "input"
+    siblingArray.push(siblingAgeInput);
+
+    // Find the Add button and insert the new sibling labels and inputs before it
+    let location = document.getElementById("addSiblingButton");
+    for (let i = 0; i < siblingArray.length; i++)
+    {
+        location.parentNode.insertBefore(siblingArray[i], location);
+    }
+
 }
 addSibling()
             //<label for = "sibling1" class = "label">Sibling:</label>
