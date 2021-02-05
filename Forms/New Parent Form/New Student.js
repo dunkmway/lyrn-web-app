1f0a5763107bb016bfd904fb895bb3fb02d63726
Matthew15243 <matthew@amplifycp.com>
2021-02-05 14:16:42 -0700

    Amazing Image!!!

--- Forms/New Parent Form/New Student.js
+++ Forms/New Parent Form/New Student.js
@@ -35,29 +35,30 @@ function createElements(elementType = [], classes = [], attributes = [], values 
 
 
   function createParentForm() {
+    let formDiv = createElement("div", "flexColumn", ["id"], ["formDiv"], "")
     let questions = []
     let parentLocation = document.getElementById("form");
 
     // Title
-    questions.push(createElement("h2", "formTitle", [], [], "Parent Information"))
+    parentLocation.parentNode.insertBefore(createElements(["h2"], ["formTitle"], [[]], [[]], ["Parent Information"]), parentLocation);
 
     // First Name
-    questions.push(createElements(["label", "input"], ["label", "input"], [["for"], ["id"]], [["firstName"], ["firstName"]], ["First Name:", ""]));
+    questions.push(createElements(["label", "input"], ["label", "input"], [["for"], ["id", "placeholder"]], [["firstName"], ["firstName", "First Name"]], ["First Name:", ""], "flexColumn"));
 
     // Last Name
-    questions.push(createElements(["label", "input"], ["label", "input"], [["for"], ["id"]], [["lastName"], ["lastName"]], ["Last Name:", ""]));
+    questions.push(createElements(["label", "input"], ["label", "input"], [["for"], ["id", "placeholder"]], [["lastName"], ["lastName", "Last Name"]], ["Last Name:", ""], "flexColumn"));
 
     // Phone Number
-    questions.push(createElements(["label", "input"], ["label", "input"], [["for"], ["id"]], [["phoneNumber"], ["phoneNumber"]], ["Phone Number:", ""]));
+    questions.push(createElements(["label", "input"], ["label", "input"], [["for"], ["id", "placeholder"]], [["phoneNumber"], ["phoneNumber", "Phone Number"]], ["Phone Number:", ""], "flexColumn"));
 
     // Email Address
-    questions.push(createElements(["label", "input"], ["label", "input"], [["for"], ["id"]], [["email"], ["email"]], ["Email Address:", ""]));
+    questions.push(createElements(["label", "input"], ["label", "input"], [["for"], ["id", "placeholder"]], [["email"], ["email", "E-mail Address"]], ["Email Address:", ""], "flexColumn"));
 
     // Place of Employment
-    questions.push(createElements(["label", "input"], ["label", "input"], [["for"], ["id"]], [["employment"], ["employment"]], ["Place of Employment:", ""]));
+    questions.push(createElements(["label", "input"], ["label", "input"], [["for"], ["id", "placeholder"]], [["employment"], ["employment", "Place of Employment"]], ["Place of Employment:", ""], "flexColumn"));
 
     // How they heard of Wasatch Tutors
-    questions.push(createElements(["label", "input"], ["label finalInput", "input finalInput"], [["for"], ["id"]], [["marketing"], ["marketing"]], ["How did you hear about us:", ""]));
+    questions.push(createElements(["label", "input"], ["label finalInput", "input finalInput"], [["for"], ["id", "placeholder"]], [["marketing"], ["marketing", "How did you hear about us?"]], ["How did you hear about us:", ""], "flexColumn"));
 
     // Agreement Section
     questions.push(createElement("div", "", ["id"], ["agreementDiv"]));
@@ -67,7 +68,7 @@ function createElements(elementType = [], classes = [], attributes = [], values 
     questions[questions.length - 1].querySelector("ul").appendChild(createElements(["li", "input", "text"], ["", "input miniBox", ""], [[], [], []], [[], [], []], ["There is a $30 fee for all returned checks.", "", "(initials)"]));
     questions[questions.length - 1].querySelector("ul").appendChild(createElements(["li", "input", "text"], ["", "input miniBox", ""], [[], [], []], [[], [], []], ["Payment is due on the 1st of each month and will be for the total amount for the month to come. (Session must be paid, prior to instruction)", "", "(initials)"]));
     questions[questions.length - 1].querySelector("ul").appendChild(createElements(["li", "input", "text"], ["", "input miniBox", ""], [[], [], []], [[], [], []], ["Notice must be given by the end of the previous business day for <u>ALL</u> cancellations and/or schedule changes.", "", "(initials)"]));
-    questions[questions.length - 1].querySelector("ul").appendChild(createElements(["li", "input", "text"], ["", "input miniBox", ""], [[], [], []], [[], [], []], ["If notice is not given the day prior, we understand that we <em>WILL</em> be charged for the full amount of the session.", "", "(initials)"]));
+    questions[questions.length - 1].querySelector("ul").appendChild(createElements(["li", "input", "text"], ["", "input miniBox", ""], [[], [], []], [[], [], []], ["If notice is not given the day prior, I understand that I <em>WILL</em> be charged for the full amount of the session.", "", "(initials)"]));
 
     let elements = createElement("div", "flexRow");
     elements.appendChild(createElements(["input", "text"], ["input signature", "signature"], [[], []], [[], []], ["", "Parent Signature"], "flexColumn"));
@@ -80,9 +81,10 @@ function createElements(elementType = [], classes = [], attributes = [], values 
 
     // Add the questions / elements to the form div
     for (let i = 0; i < questions.length; i++) {
-      parentLocation.appendChild(questions[i]);
+      formDiv.appendChild(questions[i]);
     };
 
+      parentLocation.appendChild(formDiv);
   }
 
   createParentForm();
