13edd350a2f820cdd60e011aac21f9a952454e1e
Matthew15243 <matthew15243@gmail.com>
2021-04-05 17:38:04 -0600

    comments

--- public/Site Templates/generalFunctions.js
+++ public/Site Templates/generalFunctions.js
@@ -1,3 +1,8 @@
+/**
+ * Parse the datetime as an integer into a datetime object
+ * @param {Integer} date Datetime represented as an integer
+ * @returns {Object} a javascript object containing the datetime elements
+ */
 function convertFromDateInt(date) {
 
     // Make sure an integer was passed in
@@ -5,28 +10,30 @@ function convertFromDateInt(date) {
         return undefined;
     }
 
+    // Create the date object from the date as an integer
     current_date = new Date(date)
 
+    // create the variables that will be called more than once
     const year = current_date.getFullYear();
     const month = current_date.getMonth() + 1;
-    const dayOfMonth = current_date.getDate();
-    const hours = current_date.getHours();
-    const minutes = current_date.getMinutes();
-    const seconds = current_date.getSeconds();
     const dayOfWeek = current_date.getDay() + 1;
+    const hours = current_date.getHours();
 
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    // Needed to get the month and day string values
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
+    // Create and return the datetime object
     return {'year' : year,
             'monthNumber' : month,
             'monthString' : months[month - 1],
-            'dayOfMonth' : dayOfMonth,
+            'dayOfMonth' : current_date.getDate(),
             'dayOfWeekNumber' : dayOfWeek,
             'dayOfWeekString' : days[dayOfWeek - 1],
             'hours' : hours > 12 ? hours - 12 : hours,
             'militaryHours' : hours,
-            'minutes' : minutes,
-            'seconds' : seconds,
+            'minutes' : current_date.getMinutes(),
+            'seconds' : current_date.getSeconds(),
+            'milliseconds' : current_date.getMilliseconds(),
             'shortDate' : month.toString() + "/" + dayOfMonth.toString() + "/" + year.toString()};
 }
