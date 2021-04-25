e8f199f5c325ce16e37b6d73b18251caa2af15a9
Duncan Morais <duncanmorais@gmail.com>
2021-04-24 20:46:24 -0600

    display name madness

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -38,6 +38,82 @@ admin.initializeApp();
     });
  });
 
+ exports.setUserDisplayName = functions.https.onRequest((request, response) => {
+    const listAllUsers = (nextPageToken) => {
+        // List batch of users, 1000 at a time.
+        admin.auth().listUsers(1000, nextPageToken)
+        .then((listUsersResult) => {
+            let promises = [];
+            listUsersResult.users.forEach((userRecord) => {
+                //get the user's role
+                let role = userRecord.customClaims.role;
+                //use this to get their collection and get their first and last name
+                let Role = role.charAt(0).toUpperCase() + role.slice(1) + 's';
+                const collection = Role;
+                const firstNameKey = role + "FirstName";
+                const lastNameKey = role + "LastName";
+
+                const userProfileRef = admin.firestore().collection(collection).doc(userRecord.uid);
+                let promise =  userProfileRef.get()
+                .then((doc) => {
+                    if (doc.exists) {
+                        //use this string to set their display name;
+                        const firstName = doc.data()[firstNameKey];
+                        const lastName = doc.data()[lastNameKey];
+
+                        const displayName = firstName + " " + lastName;
+
+                        return admin.auth().updateUser(userRecord.uid, {
+                            displayName : displayName
+                        })
+                        .then((userRecord) => {
+                            // response.send("Success giving user " + userRecord.uid + " display name " + userRecord.displayName);
+                            console.log("Success giving user " + userRecord.uid + " display name " + userRecord.displayName);
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                            // response.send(error);
+                        });
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                    // response.send(error);
+                });
+                promises.push(promise);
+            });
+            if (listUsersResult.pageToken) {
+                // List next batch of users.
+                listAllUsers(listUsersResult.pageToken);
+            }
+            return Promise.all(promises)
+            .then(() => {
+                response.send("Success!")
+            })
+        })
+        .catch((error) => {
+            console.log('Error listing users:', error);
+            response.send(error);
+        });
+    };
+    
+    // Start listing users from the beginning, 1000 at a time.
+    listAllUsers()
+});
+
+exports.getUserDisplayName = functions.https.onCall((data, context) => {
+    const promise = new Promise((resolve, reject) => {
+        admin.auth().getUser(data.uid)
+        .then((userRecord) => {
+            resolve(userRecord.displayName);
+        })
+        .catch((error) => {
+            reject(new functions.https.HttpsError(error.code, error.message, error.details));
+        });
+    });
+    return promise;
+});
+
  exports.addUser = functions.https.onCall((data, context) => {
     const promise = new Promise((resolve, reject) => {
         if (context.auth) {
@@ -85,8 +161,8 @@ admin.initializeApp();
     return promise;
 });
 
-exports.sendErrorReport = functions.https.onCall((data, context) => {
-    const errorRef = admin.firestore().collection("Error-Reports").doc();
-    errorRef.set(data.report)
-    .then().catch();
-});
+// exports.sendErrorReport = functions.https.onCall((data, context) => {
+//     const errorRef = admin.firestore().collection("Error-Reports").doc();
+//     errorRef.set(data.report)
+//     .then().catch();
+// });
