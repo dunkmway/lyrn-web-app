9456e54b997618f7cec94946ea7c9477d7a77fab
Duncan Morais <duncanmorais@gmail.com>
2021-05-19 09:22:59 -0600

    stuff

--- public/Dashboard/Tutor.js
+++ public/Dashboard/Tutor.js
@@ -24,87 +24,100 @@ function initialSetupData() {
 }
 
 function setStudentTable() {
-  const locationDocRef = firebase.firestore().collection("Locations").doc(currentLocation)
-  locationDocRef.get()
-  .then((doc) => {
-    if (doc.exists) {
-      let locationName = doc.get("locationName");
-      //document.getElementById("locationName").textContent = locationName;
+  let tableData = [];
+  let promises = []
+  let currentLocations = ["mhOjmqiieW6zrHcvsElp", "tykwKFrvmQ8xg2kFfEeA"]
+  for (let i = 0; i < currentLocations.length; i++) {
+    const locationDocRef = firebase.firestore().collection("Locations").doc(currentLocations[i])
+    let locationProm = locationDocRef.get()
+    .then((doc) => {
+      if (doc.exists) {
+        let locationName = doc.get("locationName");
+        let activeStudents = doc.get("activeStudents");
 
-      let activeStudents = doc.get("activeStudents");
-
-
-      let tableData = [];
-
-      if (activeStudents) {
-        for (const studentUID in activeStudents) {
-          const student = {
-            ...activeStudents[studentUID],
-            studentUID: studentUID,
-            status: "active"
-          }
-          //adjust type to be readable
-          if (student.studentType == 'act') {
-            student.studentType = 'ACT'
-          }
-          else if (student.studentType == 'subject-tutoring') {
-            student.studentType = 'ST'
-          }
-          else if (student.studentType == 'math-program') {
-            student.studentType = 'Math Program'
-          }
-          else if (student.studentType == 'phonics-program') {
-            student.studentType = 'Phonics Program'
-          }
-          else {
-            student.studentType == "";
-          }
-          tableData.push(student);
-        }
-      }
-
-      let studentTable = $('#student-table').DataTable( {
-        data: tableData,
-        columns: [
-          { data: 'studentFirstName' },
-          { data: 'studentLastName' },
-          { data: 'studentType'},
-        ],
-        "scrollY": "400px",
-        "scrollCollapse": true,
-        "paging": false
-      } );
-
-      studentTable.on('click', (args1) => {
-        let studentUID = tableData[args1.target._DT_CellIndex.row].studentUID;
-        let status = tableData[args1.target._DT_CellIndex.row].status;
-        let type = tableData[args1.target._DT_CellIndex.row].studentType;
-
-        switch (status) {
-          case "active":
-            if (type == 'ACT') {
-              actStudentSelected(studentUID);
+        if (activeStudents) {
+          for (const studentUID in activeStudents) {
+            const student = {
+              ...activeStudents[studentUID],
+              studentUID: studentUID,
+              status: "active",
+              location: locationName,
             }
-            else if (type == 'ST') {
-              subjectTutoringStudentSelected(studentUID);
+            //adjust type to be readable
+            if (student.studentType == 'act') {
+              student.studentType = 'ACT'
             }
-            //FIXME: these will need to be redirected to the proper page once we have them
-            else if (type == 'Math Program') {
-              subjectTutoringStudentSelected(studentUID);
+            else if (student.studentType == 'subject-tutoring') {
+              student.studentType = 'ST'
             }
-            else if (type == 'Phonics Program') {
-              subjectTutoringStudentSelected(studentUID);
+            else if (student.studentType == 'math-program') {
+              student.studentType = 'Math Program'
+            }
+            else if (student.studentType == 'phonics-program') {
+              student.studentType = 'Phonics Program'
             }
             else {
-              alert("nothing to see here...yet")
+              student.studentType == "";
             }
-          break;
+
+            //adjust name to be last, first
+            student.studentName = student.studentLastName + ", " + student.studentFirstName
+
+            tableData.push(student);
+          }
+        }
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      handleFirebaseErrors(error, document.currentScript.src);
+    });
+    promises.push(locationProm);
+  }
+
+  return Promise.all(promises)
+  .then(() => {
+    let studentTable = $('#student-table').DataTable( {
+      data: tableData,
+      columns: [
+        { data: 'studentName' },
+        { data: 'studentType'},
+        { data: 'location'}
+      ],
+      "scrollY": "400px",
+      "scrollCollapse": true,
+      "paging": false
+    } );
+
+    studentTable.on('click', (args1) => {
+      let studentUID = tableData[args1.target._DT_CellIndex.row].studentUID;
+      let status = tableData[args1.target._DT_CellIndex.row].status;
+      let type = tableData[args1.target._DT_CellIndex.row].studentType;
+
+      switch (status) {
+        case "active":
+          if (type == 'ACT') {
+            actStudentSelected(studentUID);
+          }
+          else if (type == 'ST') {
+            subjectTutoringStudentSelected(studentUID);
+          }
+          //FIXME: these will need to be redirected to the proper page once we have them
+          else if (type == 'Math Program') {
+            subjectTutoringStudentSelected(studentUID);
+          }
+          else if (type == 'Phonics Program') {
+            subjectTutoringStudentSelected(studentUID);
+          }
+          else {
+            alert("nothing to see here...yet")
+          }
+        break;
         default:
           console.log("ERROR: This student isn't active or pending!!!")
-        }
-        
-      })
-    }
+      }
+      
+    });
   })
   .catch((error) => {
     console.log(error);
