44606bab17a6d72271f997062b1fa5f06216017e
Duncan Morais <duncanmorais@gmail.com>
2021-08-24 21:20:14 -0600

    better chat input

--- public/Site Templates/student-chat.js
+++ public/Site Templates/student-chat.js
@@ -236,3 +236,38 @@ function sendStudentMessage(studentUID, studentType, conversationType, message, 
     });
   });
 }
+
+//text area auto resize
+var observe;
+if (window.attachEvent) {
+    observe = function (element, event, handler) {
+        element.attachEvent('on'+event, handler);
+    };
+}
+else {
+    observe = function (element, event, handler) {
+        element.addEventListener(event, handler, false);
+    };
+}
+function initTextAreaResize () {
+    document.querySelectorAll('.studentMessagesInput').forEach(text => {
+      function resize () {
+        text.style.height = 'auto';
+        text.style.height = (text.scrollHeight + 10)+'px';
+      }
+      /* 0-timeout to get the already changed text */
+      function delayedResize () {
+          window.setTimeout(resize, 0);
+      }
+      observe(text, 'change',  resize);
+      observe(text, 'cut',     delayedResize);
+      observe(text, 'paste',   delayedResize);
+      observe(text, 'drop',    delayedResize);
+      observe(text, 'keydown', delayedResize);
+
+      text.focus();
+      text.select();
+      resize();
+    })
+}
+document.body.addEventListener('load', initTextAreaResize());
